8b97d7e2813867a79d9cddd6343fd7a2703b30be
Mark Klocek <Mark.Klocek@GMail.com>
2025-06-25 11:04:56 -0700

    set up node server

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 8000;
 
 
-app.listen(8000)
+app.get('/',(request,response)=>{
+    response.sendFile(__dirname + '/index.html')
+})
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
