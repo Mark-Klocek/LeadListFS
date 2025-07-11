e511df475d56d5d829b9911dac1735d0bb8c91f6
Mark Klocek <Mark.Klocek@GMail.com>
2025-07-11 08:18:07 -0700

    fixed routing to styling and js files

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ const PORT = process.env.PORT || 8000;
 app.get('/',(request,response)=>{
     response.sendFile(__dirname + '/index.html')
 })
+app.get('/css/normalize.css',(request,response)=>{
+    response.sendFile(__dirname + '/css/normalize.css')
+})
+app.get('/css/style.css',(request,response)=>{
+    response.sendFile(__dirname + '/css/style.css')
+})
+app.get('/js/main.js',(request,response)=>{
+    response.sendFile(__dirname + '/js/main.js')
+})
 
 
 
