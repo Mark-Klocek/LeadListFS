790626b8ce2da0f32d1f3eb321caebf005309a08
Mark Klocek <Mark.Klocek@GMail.com>
2025-07-15 10:43:24 -0700

    changing logic so that express methods arent all within mongo conection promise - only the app starting is

--- server.js
+++ server.js
@@ -10,40 +10,27 @@ app.use(express.static('public'))
 app.use(express.json())
 
 
-//connect to db
+//after db connection returning main index
+app.get('/',(request,response)=>{
+    response.sendFile(__dirname + '/index.html')
+})
+
+
+
+
+
+
+//connect to db and start app
 MongoClient.connect(connectString)
     //after db connection
     .then( client =>{
         console.log('connected to mongodb')
         const db = client.db('LeadLis')
         const businessCollection = db.collection('1')
-        const businessArray = businessCollection.find().toArray()
-            
-            
-        //after db connection returning main index
-        app.get('/',(request,response)=>{
-        response.sendFile(__dirname + '/index.html')
-        })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        //const businessArray = businessCollection.find().toArray()
         app.listen(PORT,()=>{
         console.log(`Server is running on port ${PORT}`)
         })
-        
-        
-
     })
     .catch(error=>{
         console.error(error)
